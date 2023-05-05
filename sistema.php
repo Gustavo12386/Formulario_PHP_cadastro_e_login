ae422deb2eda3c905b28af3a8fcf0841af1c0be0
Gustavo12386 <gucalderaro@hotmail.com>
2023-05-05 15:27:18 -0300

    primeiro commit

---
+++ sistema.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+//print_r($_SESSION);
+if((!isset($_SESSION['email']) == true) and (!isset($_POST['senha']) == true))
+{
+   unset($_SESSION['email']);
+   unset($_SESSION['senha']); 
+   header('Location: login.php');    
+}
+$logado = $_SESSION['email'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+    <title>Sistema</title>
+    <style>
+       body{
+        background: linear-gradient(to right, rgb(20, 147, 220), rgb(17,54,71)); 
+        color: white; 
+        text-align: center;    
+       }
+    </style>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div class="container-fluid">
+         <a class="navbar-brand" href="#">Formulario PHP</a>
+         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span>
+         </button>
+      </div>
+      <div class="d-flex">
+        <a href="sair.php" class="btn btn-danger me-5">Sair</a>
+      </div>
+    </nav>
+    <?php
+     echo "<h1>Bem vindo <u>$logado</u></h1>";
+    ?>
+</body>
+</html>
